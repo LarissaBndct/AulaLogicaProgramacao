9e04b203e83a73bdd2e2f8881e9fa26c07ee9052
LarissaBndct <larissagomes0119@gmail.com>
2024-08-26 21:00:16 -0300

    ajuste

--- ex8.py
+++ ex8.py
@@ -1,6 +1,6 @@
 import os
 eletrodomesticos = []
-
+tarifa = 0
 while True:
     consumo = input("Informe o  consumo de energia em kWh de cada eletrodoméstico ou digite 's' para sair ")
     if consumo == 's' or consumo == 'S':
@@ -10,13 +10,14 @@ while True:
         if "" == consumo:
             print("Informe um valor válido")
         else:
+            consumo = float(consumo)
             eletrodomesticos.append(consumo)
 
-soma = sum(eletrodomesticos)
-print(f"Cosumo total sem tarifa: {soma}")
-# To do:
-#5.Aplicar a tarifa de acordo com o consumo total:
-#-Se o consumo for até 100 kWh, a tarifa é de R$0,50 por kWh.
-#-Se o consumo for entre 101 e 200 kWh, a tarifa é de R$0,75 por kWh.
-#-Se o consumo for acima de 200 kWh, a tarifa é de R$1,00 por kWh.
-#6.Exibir o consumo total e o valor a ser pago.
+somaConsumo = sum(eletrodomesticos)
+if somaConsumo <= 100:
+    tarifa = somaConsumo * 0.5
+elif somaConsumo <= 200:
+    tarifa = somaConsumo * 0.75
+elif somaConsumo > 200:
+    tarifa = somaConsumo 
+print(f"Consumo total: {somaConsumo}kWh. Total a pagar: R${tarifa:.2f}")
